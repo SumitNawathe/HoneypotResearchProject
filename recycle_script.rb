0ca91f610c9b2ce552742beb25e3d2044b957d44
Sumit Nawathe <sumit.nawathe@gmail.com>
2022-10-01 20:48:05 +0000

    process exists correctly

--- recycle_script.rb
+++ recycle_script.rb
@@ -1,5 +1,3 @@
-# ROUGH OUTLINE
-
 require 'rufus-scheduler'
 require './network'
 require './ssh_key_utils'
@@ -10,15 +8,14 @@ require './mitm'
 
 # create network
 # TODO: must be randomized
-n = Network.create_fresh(3, "prefix")
+n = Network.create_fresh(3, "prefix") # TODO hardcoded
 n.create_and_start_all
 puts "== LOG == created network"
 sleep(5)
 
 # create MITM
-external_ip = "128.8.238.197"
+external_ip = "128.8.238.197" # TODO hardcoded
 port = MITM.get_port_from_external_ip(external_ip)
-puts "== LOG == port=#{port}"
 mitm = MITM.new(n, port)
 puts "== LOG == created mitm"
 sleep(3)
@@ -26,7 +23,7 @@ sleep(3)
 # connect MITM to router container and external IP
 initialize_ssh(n.router, "password")
 sleep(3)
-mitm.start("~/mitm.log")
+mitm.start("~/mitm.log") # TODO hardcoded
 sleep(3)
 mitm.connect_to_external_ip(external_ip)
 sleep(3)
@@ -35,28 +32,32 @@ puts "== LOG == started mitm, connected to external ip"
 # container ssh linking
 n.containers.each_with_index do |container, index|
   initialize_ssh(container)
-  puts "== LOG == initialize container #{index} ssh"
   place_public_key(n.router, container)
-  puts "== LOG == placed public key for container #{index}"
   add_alias(n.router, container, "machine#{index}")
-  puts "== LOG == added alias for container #{index} to router"
+  puts "== LOG == connected container #{index} to router"
 end
 
 # container NAT stuff
+# TODO
 
-# 20 min timer after attacker enters
+# wait until attacker enters
+# TODO
+
+# create timer to destroy honeypot
 scheduler = Rufus::Scheduler.new
 scheduler.in '1m' do
   # TODO: retrieve logs and attacker information before destroying
 
-  n.stop_and_destroy_all
   mitm.disconnect_from_external_ip(external_ip)
   mitm.stop
-  puts "Network's containers have been stopped and destroyed"
+  `rm -f ~/mitm.log`
+  n.stop_and_destroy_all
+  puts "== LOG == honeypot destroyed"
 
   # TODO: call recycle script again
+  
+  exit(0)
 end
 scheduler.join
 
-puts "Done"
 
