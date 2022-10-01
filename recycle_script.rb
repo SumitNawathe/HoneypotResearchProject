d456153b3eedca985557f68bf0251adf449cc2e4
Sumit Nawathe <sumit.nawathe@gmail.com>
2022-10-01 20:35:55 +0000

    works pretty well

--- recycle_script.rb
+++ recycle_script.rb
@@ -1,5 +1,6 @@
 # ROUGH OUTLINE
 
+require 'rufus-scheduler'
 require './network'
 require './ssh_key_utils'
 require './mitm'
@@ -11,31 +12,51 @@ require './mitm'
 # TODO: must be randomized
 n = Network.create_fresh(3, "prefix")
 n.create_and_start_all
+puts "== LOG == created network"
+sleep(5)
 
 # create MITM
 external_ip = "128.8.238.197"
 port = MITM.get_port_from_external_ip(external_ip)
+puts "== LOG == port=#{port}"
 mitm = MITM.new(n, port)
+puts "== LOG == created mitm"
+sleep(3)
 
 # connect MITM to router container and external IP
 initialize_ssh(n.router, "password")
+sleep(3)
 mitm.start("~/mitm.log")
+sleep(3)
 mitm.connect_to_external_ip(external_ip)
+sleep(3)
+puts "== LOG == started mitm, connected to external ip"
 
 # container ssh linking
 n.containers.each_with_index do |container, index|
   initialize_ssh(container)
+  puts "== LOG == initialize container #{index} ssh"
   place_public_key(n.router, container)
+  puts "== LOG == placed public key for container #{index}"
   add_alias(n.router, container, "machine#{index}")
+  puts "== LOG == added alias for container #{index} to router"
 end
 
 # container NAT stuff
 
-# wait for attacker to enter
-# 20 min timer
+# 20 min timer after attacker enters
+scheduler = Rufus::Scheduler.new
+scheduler.in '1m' do
+  # TODO: retrieve logs and attacker information before destroying
 
-mitm.disconnect_from_external_ip(external_ip)
-mitm.stop
-network.stop_and_destroy_all
+  n.stop_and_destroy_all
+  mitm.disconnect_from_external_ip(external_ip)
+  mitm.stop
+  puts "Network's containers have been stopped and destroyed"
 
-# call script again
+  # TODO: call recycle script again
+end
+scheduler.join
+
+puts "Done"
+
