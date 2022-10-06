f575c3a3e70c54930d9455cb7747e226956993e3
Amrit Magesh <amrit6543@gmail.com>
2022-10-06 20:52:31 +0000

    scraping mitm

---
+++ mitm_scrape.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIRECTORY_PATH="$1"
+
+NUM_COMMANDS=$(cat "$DIRECTORY_PATH/mitm.log" | grep "line from reader" | awk '{ print $9 }' | wc -l)
+
+cat "$DIRECTORY_PATH/mitm.log" | grep "line from reader" | cut -d' ' -f9- > "$DIRECTORY_PATH/filtered_mitm.log"
+
+
