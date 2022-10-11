41fea9f01ffd08e3a701a24cc15f9971dc69efb7
Anthony <anthony.b.huynh@gmail.com>
2022-10-11 20:51:37 +0000

    updated Date Calculations

--- dateCalculation.sh
+++ dateCalculation.sh
@@ -7,10 +7,10 @@ n1=1
 for (( c=1; c<=$pairCount; c++ ))  
 do 
    n2=$((2 * c))    	
-   entryMonthDate=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n1 | tail -1 | colrm 8`  
-   exitMonthDate=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n2 | tail -1 | colrm 8`
-   entryTimeStamp=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n1 | tail -1 | cut -d' ' -f4` 
-   exitTimeStamp=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n2 | tail -1 | cut -d' ' -f4`
+   entryMonthDate=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n1 | tail -1 | colrm 7`  
+   exitMonthDate=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n2 | tail -1 | colrm 7`
+   entryTimeStamp=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n1 | tail -1 | awk '{print $3}'` 
+   exitTimeStamp=`cat $1 | grep "sshd\[.*\]: pam_unix(sshd:session)" | head -$n2 | tail -1 | awk '{print $3}'`
    entryMonthDateStamp=`date -d"$entryMonthDate" "+%Y-%m-%d"`
    exitMonthDateStamp=`date -d"$exitMonthDate" "+%Y-%m-%d"`
    entryStamp=`date -d"$entryMonthDateStamp $entryTimeStamp" +%s`
