745deb435ce470d3fd6981945a3ade0a2ec94502
Sumit Nawathe <sumit.nawathe@gmail.com>
2022-10-14 03:19:22 +0000

    healthcare honey generation

---
+++ honey_healthcare.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# assume sudo (in lxc-attach)
+# move to home directory
+cd
+
+# install java and synthea
+sudo apt-get install git -y
+sudo apt-get install default-jre -y
+sudo apt-get install wget -y
+wget https://github.com/synthetichealth/synthea/releases/download/master-branch-latest/synthea-with-dependencies.jar
+
+# create config file
+touch config
+echo "exporter.csv.export = true" > config
+
+# run synthea
+NUM_PATIENTS=$(( ( $RANDOM % 200 ) + 300 ))
+java -jar synthea-with-dependencies.jar Maryland -c ./config -p "$NUM_PATIENTS"
+
+# pull csv export to home, delete rest
+cp ./output/csv/* .
+rm config
+rm synthea-with-dependencies.jar
+sudo rm -rf output
+
+
