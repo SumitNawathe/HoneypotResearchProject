220101c0e1b52719e76bdc494090d1342629a574
Sumit Nawathe <sumit.nawathe@gmail.com>
2022-10-15 23:34:17 +0000

    working

--- create_honey_snapshots.rb
+++ create_honey_snapshots.rb
@@ -3,16 +3,19 @@
 require './container'
 require './ssh_key_utils'
 
-for i in 0...5
+for i in 0...2
   # create container
   c = Container.new("healthcare#{i}")
   c.create.start
+  sleep(3)
 
   # initialize ssh to allow file transfer
   initialize_ssh(c)
+  sleep(3)
 
   # perform honey creation
   c.create_honey 'honey_healthcare.sh'
+  sleep(3)
 
   # create snapshot
   c.stop
