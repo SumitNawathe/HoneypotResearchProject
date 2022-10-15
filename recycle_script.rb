b651260fd19fd77e6173c6253a455516b60dd4eb
Sumit Nawathe <sumit.nawathe@gmail.com>
2022-10-15 20:23:10 +0000

    recycle script logs to separate file

--- recycle_script.rb
+++ recycle_script.rb
@@ -6,6 +6,7 @@ require './ssh_key_utils'
 require './mitm'
 require './data_utils'
 require './nat_rules'
+require './logger'
 
 # param: ip address
 if ARGV.length != 1
@@ -13,6 +14,8 @@ if ARGV.length != 1
   exit(1)
 end
 EXTERNAL_IP = ARGV[0]
+logger = HoneypotLogger.new(EXTERNAL_IP)
+logger.log "beginning recycling script"
 
 # get relevant directories
 HOME_DIR = `cd && pwd`.chomp
@@ -20,17 +23,17 @@ HONEYPOT_DIR = "#{HOME_DIR}/#{EXTERNAL_IP}_files"
 `mkdir #{HONEYPOT_DIR}`
 
 # create network
-network_size = [5, 10].sample
+network_size = [3, 6].sample
 n = Network.create_fresh(network_size, "prefix")
 n.create_and_start_all
 n.write_to_file "#{HONEYPOT_DIR}/network_layout.txt"
-puts "== LOG == created network"
+logger.log "created network size=#{network_size}"
 sleep(5)
 
 # create MITM
 port = MITM.get_port_from_external_ip(EXTERNAL_IP)
 mitm = MITM.new(n, port)
-puts "== LOG == created mitm"
+logger.log "created mitm"
 sleep(3)
 
 # connect MITM to router container and external IP
@@ -40,42 +43,44 @@ mitm.start("#{HONEYPOT_DIR}/mitm.log")
 sleep(3)
 mitm.connect_to_external_ip(EXTERNAL_IP)
 sleep(3)
-puts "== LOG == started mitm, connected to external ip"
+logger.log "started mitm, connected to external ip"
 
 # container ssh linking
 n.containers.each_with_index do |container, index|
   initialize_ssh(container)
   place_public_key(n.router, container)
   add_alias(n.router, container, "machine#{index}")
-  puts "== LOG == connected container #{index} to router"
+  logger.log "connected container #{index} to router"
 end
 
 # create log files
 n.redirect_auth_logs_to HONEYPOT_DIR
-puts "== LOG == auth log files redirected"
-puts "== LOG == ready to accept attackers"
+logger.log "auth log files redirected"
+logger.log "ready to accept attackers"
 
 # wait until attacker enters
 `sudo chmod a+r #{HONEYPOT_DIR}/mitm.log`
 `sudo tail -n 0 -f "#{HONEYPOT_DIR}/mitm.log" | grep -Eq "uthenticated"`
-puts "== LOG == attacker entered"
+logger.log "attacker entered"
 
 # create timer to destroy honeypot
 scheduler = Rufus::Scheduler.new
 scheduler.in '1m' do
+  logger.log "beginning honeypot destruction"
+
   # disconnect mitm (kick attacker out)
   mitm.disconnect_from_external_ip(EXTERNAL_IP)
   mitm.stop
-  puts "=== LOG === mitm stopped"
+  logger.log "mitm stopped"
 
   # retrieve and package logs/data
   package_honeypot_data(EXTERNAL_IP)
   clear_honeypot_dir(EXTERNAL_IP)
-  puts "=== LOG === logs retrieved"
+  logger.log "logs retrieved"
 
   # stop honeypot containers
   n.stop_and_destroy_all
-  puts "== LOG == honeypot destroyed"
+  logger.log "honeypot destroyed"
 
   # TODO: call recycle script again
   
