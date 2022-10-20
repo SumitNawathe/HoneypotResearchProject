b3e6de12320700463f79cd24f06551af63f55af4
Amrit Magesh <69167269+code890@users.noreply.github.com>
2022-10-20 16:46:25 -0400

    Update mitm_scrape.sh

--- mitm_scrape.sh
+++ mitm_scrape.sh
@@ -4,7 +4,10 @@ DIRECTORY_PATH="$1"
 
 NUM_COMMANDS=$(cat "$DIRECTORY_PATH/mitm.log" | grep "line from reader" | awk '{ print $9 }' | wc -l)
 
-cat "$DIRECTORY_PATH/mitm.log" | grep "line from reader" | cut -d' ' -f9- > "$DIRECTORY_PATH/mitm_commands.processed"
+NON_INTERACTIVE_COMMANDS=$(cat "$DIRECTORY_PATH/mitm.log" | grep "Noninteractive mode attacker command" | awk '{ print $10 }' | wc -l)
+
+NUM_COMMANDS=$(( NUM_COMMANDS + NON_INTERACTIVE_COMMANDS ))
+
+cat "$DIRECTORY_PATH/mitm.log" | grep -e "line from reader" -e "Noninteractive mode attacker command" | cut -d':' -f4- | sed 's/^ *//g' > "$DIRECTORY_PATH/mitm_commands.processed"
 
 echo "$NUM_COMMANDS"
-
