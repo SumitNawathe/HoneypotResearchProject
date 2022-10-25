9a85eee2a8ce3b810229f736d7edb455c794fa36
4nth0n7 <114354410+4nth0n7@users.noreply.github.com>
2022-10-25 13:04:18 -0400

    Update watch-incoming.sh

--- watch-incoming.sh
+++ watch-incoming.sh
@@ -10,8 +10,8 @@ LOG=/home/student/processed_data/"processed_$1.log"
 inotifywait -m -e create -e moved_to --format "%f" $TARGET \
         | while read FILENAME
                 do
-                        mv $TARGET/$FILENAME $TRACKING/$FILENAME
-                        tar xvzf $TRACKING/$FILENAME -C $DESTINATION/$FILENAME
+                        cp $TARGET/$FILENAME $TRACKING/$FILENAME
+                        tar xvzf $TRACKING/$FILENAME -C $DESTINATION
                         cat $DESTINATION/duration.processed >> $LOG
                         cat $DESTINATION/mitm_commands.processed >> $LOG
                         echo "--------------------------------------------------------" >> $LOG
